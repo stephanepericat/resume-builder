e07830a42600b6cee11b11ae2c309067f5b12b84
Stephane P. Pericat <contact@pericat.net>
2025-04-29 08:01:28 -0500

    chore: add robots tag header

--- apps/ui/next.config.ts
+++ apps/ui/next.config.ts
@@ -1,7 +1,19 @@
 import { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  async headers() {
+    return[{
+      headers: [
+        {
+          key: 'X-Robots-Tag',
+          value: 'noindex, nofollow',
+        },
+      ],
+      source: '/:path*',
+    }];
+  }
+}
 
 const withNextIntl = createNextIntlPlugin()
 export default withNextIntl(nextConfig)
